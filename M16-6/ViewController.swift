0fa0c598b191db58098dcd21f1d82104c1482f08
Serjio Alvarez <yashalavasha@gmail.com>
2024-07-30 19:09:15 +0300

    Initial Commit

---
+++ M16-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16-6
+//
+//  Created by Serjio Alvarez on 30.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
